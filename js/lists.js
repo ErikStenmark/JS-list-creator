3ae93c6b3e0339bcc76d89ea1c5ab5347cb0fcba
Erik Stenmark <ninja599@users.noreply.github.com>
2018-07-26 09:51:58 +0300

    Update lists.js

--- js/lists.js
+++ js/lists.js
@@ -5,7 +5,7 @@ for (let i = 0; i < lis.length; i++) {
   attachListItemButtons(lis[i]);
 
   lis[i].addEventListener('click', (e) => {
-    if (event.target.className == 'remove') {
+    if (e.target.className == 'remove') {
       if (confirm("are you sure you want to delete this list?")) {
         let li = e.target.parentNode.parentNode;
         let ul = e.target.parentNode.parentNode.parentNode;
